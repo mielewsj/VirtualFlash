9294fa0d2a76be87026065d8b3f55955b8e4bb79
Mielewski <mielewsj@go.stockton.edu>
2021-09-27 18:30:58 -0400

    update 1

--- numbers/src/numbers/Numbers.java
+++ numbers/src/numbers/Numbers.java
@@ -2,8 +2,8 @@ package numbers;
 
 public class Numbers {
 
-	private double[] numbers = {1.11, 2.22, 3.33, 4.44};
-	private int size = 4;
+	private double[] numbers = {1.11, 2.22, 3.33, 4.44, 5.55, 6.66, 7.77};
+	private int size = 7;
 	
 	public void removeNumber(int pos) {
 		for (int i=pos+1; i<size; i++) {
@@ -18,5 +18,25 @@ public class Numbers {
 
 	public int getSize() {
 		return size;
+	}
+	
+	public boolean addNumber(int pos, double addedNumber) {
+		boolean result = true;
+		if (numbers[size-1] != 0.00) {
+			result = false;
+		} else {
+			int top = size-1;
+			int i = top;
+			while (numbers[i] == 0.00) {
+				i--;
+			}
+			top = i;
+			for (int j=top; j>pos-1; j--) {
+				numbers[j+1] = numbers[j];
+			}
+			numbers[pos] = addedNumber;
+			result = true;
+		}
+		return result;
 	}
 }
--- numbers/src/numbers/TestNumbers.java
+++ numbers/src/numbers/TestNumbers.java
@@ -7,9 +7,15 @@ public class TestNumbers {
 		Numbers num = new Numbers();
 		int size = num.getSize();
 		double[] numbers = num.getNumbers();
-		num.removeNumber(1);
+		num.removeNumber(2);
 		for (int i=0; i<size; i++) {
 			System.out.println(numbers[i]);
+		}
+		if (num.addNumber(4, 9.99)) {
+			for (int i=0; i<size; i++) {
+				System.out.println(numbers[i]);
+			}
+
 		}
 	}
 
